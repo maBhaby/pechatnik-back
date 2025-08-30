d40a587b48a9e5bbf3c7e4ed99886029ac0635dd
maBhaby <artem_chursin_37@mail.ru>
2025-08-31 01:51:19 +0300

    feat(PB-1): make mifration for views

--- landing_views/admin.py
+++ landing_views/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PriceView
 
 # Register your models here.
+
+admin.site.register(PriceView)
--- landing_views/models.py
+++ landing_views/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class PriceView(models.Model):
+    __view_name='Блок Прайсов'
+
+    title = models.TextField(max_length=256)
+    description = models.TextField(max_length=512, blank=True, null=True)
+
+    def __str__(self):
+        return str(f'{self.title} {self.__view_name}')
